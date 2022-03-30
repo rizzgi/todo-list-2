ee1b7a7b14ad9042b649a49b4e4e0000dcf0a6bf
giofacul <giovanni.rizzardo@aluno.faculdadeimpacta.com.br>
2022-03-30 14:32:41 -0300

    add row initial

--- app2/lib/main.dart
+++ app2/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(const MaterialApp(
@@ -14,8 +17,58 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _todoList = [];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('LISTA DE TAREFAS'),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Column(children: [
+        Container(
+          padding: const EdgeInsets.fromLTRB(17, 1, 7, 7),
+        ),
+        Row(
+          children: [
+            const Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Nova tarefa",
+                  labelStyle: TextStyle(color: Colors.blueAccent),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("ADD"),
+              style: ElevatedButton.styleFrom(primary: Colors.blueAccent),
+            ),
+          ],
+        ),
+      ]),
+    );
+  }
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_todoList);
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+      return file.readAsString();
+    } catch (e) {
+      return e.toString();
+    }
   }
 }
