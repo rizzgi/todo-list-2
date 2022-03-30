c696567bb8bdea9bd96be54cf833c5c822dccebb
giofacul <giovanni.rizzardo@aluno.faculdadeimpacta.com.br>
2022-03-30 12:24:05 -0300

    create app

---
+++ app2/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
